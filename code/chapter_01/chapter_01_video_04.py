868c6e97d65cd323443f405e07b2c23ce3ed5a83
Seth Berry <seth.berry@nd.edu>
2022-11-15 13:01:50 -0500

    Code updates

--- code/chapter_01/chapter_01_video_04.py
+++ code/chapter_01/chapter_01_video_04.py
@@ -1,1 +1,30 @@
+import pandas as pd
 import numpy as np
+
+regression_data = pd.read_csv('/Users/sethberry/Documents/level-up-python-data-modeling-and-model-evaluation-metrics-2499737/data/regression_output.csv')
+
+regression_data['absolute_difference'] = abs(regression_data['actual'] - 
+  regression_data['predicted'])
+  
+regression_data['squared_error'] = (regression_data['actual'] - 
+  regression_data['predicted'])**2   
+
+mae = regression_data['absolute_difference'].mean()
+
+rmse = np.sqrt(regression_data['squared_error'].mean())
+
+mae
+
+rmse
+
+row_count = regression_data.__len__()
+mae_sum = 0
+rmse_sum = 0
+  
+for i in range(row_count):
+    mae_sum += abs(regression_data['actual'][i] - regression_data['predicted'][i])
+    rmse_sum += (regression_data['actual'][i] - regression_data['predicted'][i])**2
+  
+mae = mae_sum / row_count
+rmse = np.sqrt(rmse_sum / row_count)
+
--- code/chapter_02/chapter_02_video_01.py
+++ code/chapter_02/chapter_02_video_01.py
@@ -1,1 +1,16 @@
+import pandas as pd
 import numpy as np
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+
+work_data = pd.read_csv("/Users/sethberry/Documents/level-up-python-data-modeling-and-model-evaluation-metrics-2499737/data/level_up_data.csv")
+
+work_data.describe()
+
+work_data.info()
+
+imp_mean = IterativeImputer(random_state=0)
+
+imputed_data = imp_mean.fit_transform(work_data.to_numpy())
+
+pd.DataFrame(imputed_data).info()
