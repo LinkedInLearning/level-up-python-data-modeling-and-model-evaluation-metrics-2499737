214475a21e7c374ac7153527c15b46ec5e210c35
Seth Berry <seth.berry@nd.edu>
2022-11-15 13:13:32 -0500

    added balanced accuracy for 01_01

--- code/chapter_01/chapter_01_video_01.py
+++ code/chapter_01/chapter_01_video_01.py
@@ -3,4 +3,14 @@ FP = 20
 FN = 60
 TP = 30
 
-(TN + TP) / (TN + FP + FN + TP)
+accuracy = (TN + TP) / (TN + FP + FN + TP)
+
+accuracy
+
+sensitivity = TP / (TP + FN)
+
+specificity = TN / (TN + FP)
+
+balanced_accuracy = (sensitivity + specificity) / 2
+
+balanced_accuracy
