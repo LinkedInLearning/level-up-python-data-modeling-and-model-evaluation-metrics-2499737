649424b9173151ece9aac01d620b991605018db3
Seth Berry <seth.berry@nd.edu>
2022-11-16 15:09:28 -0500

    balancing data

--- code/chapter_02/chapter_02_video_02.py
+++ code/chapter_02/chapter_02_video_02.py
@@ -1,1 +1,24 @@
+from collections import Counter
+from imblearn.over_sampling import SMOTE
 import numpy as np
+import pandas as pd
+
+work_data = pd.read_csv("/Users/sethberry/Documents/level-up-python-data-modeling-and-model-evaluation-metrics-2499737/data/level_up_data.csv")
+
+encode_cats = pd.get_dummies(work_data[{'department'}])
+
+work_data = work_data.drop({'department'}, axis=1)
+
+work_data = work_data.join(encode_cats)
+
+predictors = work_data.drop('separatedNY', axis=1)
+
+outcome = work_data['separatedNY']
+
+Counter(outcome)
+
+oversample = SMOTE()
+
+X, y = oversample.fit_resample(predictors, outcome)  
+
+Counter(y)
