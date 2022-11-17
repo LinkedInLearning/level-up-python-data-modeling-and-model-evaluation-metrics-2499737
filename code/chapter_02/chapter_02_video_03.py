64d6cf58b8af2dc8ed14168345cc26f2549c3664
Seth Berry <seth.berry@nd.edu>
2022-11-17 09:53:25 -0500

    chapter_02_video_03 code

--- code/chapter_02/chapter_02_video_03.py
+++ code/chapter_02/chapter_02_video_03.py
@@ -1,1 +1,11 @@
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+work_data = pd.read_csv(
+  "/Users/sethberry/Documents/level-up-python-data-modeling-and-model-evaluation-metrics-2499737/data/level_up_data.csv"
+  )
+
+X_train, X_test, y_train, y_test = train_test_split(
+  X, y, test_size=0.30, random_state=1001
+  )
