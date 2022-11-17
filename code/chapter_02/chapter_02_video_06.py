4fb748e8a82218ef941376dc9cdd6da26615b4a7
Seth Berry <seth.berry@nd.edu>
2022-11-17 07:14:16 -0500

    regression updates

--- code/chapter_02/chapter_02_video_06.py
+++ code/chapter_02/chapter_02_video_06.py
@@ -1,20 +1,20 @@
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 
 # work_data = pd.read_csv("/Users/sethberry/Documents/level-up-python-data-modeling-and-model-evaluation-metrics-2499737/data/level_up_data.csv")
 
 work_data = pd.read_csv("C:/Users/sberry5/Documents/teaching/level-up-python-data-modeling-and-model-evaluation-metrics-2499737/data/level_up_data.csv")
 
-work_data = work_data.dropna()
-
 encode_cats = pd.get_dummies(work_data[{'department'}])
 
-work_data = work_data.drop({'department'}, axis = 1)
+work_data = work_data.drop({'department'}, axis=1)
 
 work_data = work_data.join(encode_cats)
 
@@ -22,7 +22,21 @@ predictors = work_data.drop('currentSalary', axis=1)
 
 outcome = work_data['currentSalary']
 
-X_train, X_test, y_train, y_test = train_test_split(predictors, outcome, test_size=0.33, random_state=42)
+imp_mean = IterativeImputer(random_state=1001)
+
+imputed_data = imp_mean.fit_transform(predictors)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    imputed_data, outcome, test_size=0.33, random_state=1001
+    )
+
+X_train_scaler = StandardScaler().fit(X_train)
+
+X_train_scaled = X_train_scaler.transform(X_train)
+
+X_test_scaler = StandardScaler().fit(X_test)
+
+X_test_scaled = X_test_scaler.transform(X_test)
 
 alpha_values = [0.1,0.3, 0.5, 0.8, 1]
 
@@ -40,11 +54,11 @@ for i in list(model_list.keys()):
   
   regression = model_list.get(i)
 
-  regression.fit(X_train, y_train)
+  regression.fit(X_train_scaled, y_train)
   
-  r2_score = regression.score(X_train, y_train)
+  r2_score = regression.score(X_train_scaled, y_train)
 
-  pred = regression.predict(X_test)
+  pred = regression.predict(X_test_scaled)
 
   mse = mean_squared_error(y_test, pred)
   
