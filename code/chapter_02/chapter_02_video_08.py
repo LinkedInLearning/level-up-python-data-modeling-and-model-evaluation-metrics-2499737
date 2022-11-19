6cbb97e976ccbcc6b2d7765337cfb4118c429f07
Seth Berry <seth.berry@nd.edu>
2022-11-19 10:05:25 -0500

    updates to 02_08

--- code/chapter_02/chapter_02_video_08.py
+++ code/chapter_02/chapter_02_video_08.py
@@ -1,28 +1,14 @@
+from joblib import load
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from sklearn.metrics import roc_auc_score, balanced_accuracy_score, matthews_corrcoef
-from sklearn.model_selection import RandomizedSearchCV, train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn import tree
 
-work_data = pd.read_csv("/Users/sethberry/Documents/level-up-python-data-modeling-and-model-evaluation-metrics-2499737/data/level_up_data.csv")
-
-encode_cats = pd.get_dummies(work_data[{'department', 'job_level'}])
-
-work_data = work_data.drop({'department', 'job_level'}, axis = 1)
-
-work_data = work_data.join(encode_cats)
-
-predictors = work_data.drop('separated_ny', axis=1)
-
-outcome = work_data['separated_ny']
-
-X_train, X_test, y_train, y_test = train_test_split(predictors, outcome, test_size=0.33, random_state=42)
-
-scaler = StandardScaler().fit(X_train)
-
-X_scaled = scaler.transform(X_train)
+X_train_scaled, X_test_scaled, y_train, y_test = load(
+  '/workspaces/level-up-python-data-modeling-and-model-evaluation-metrics-2499737/model_data.joblib'
+  )
 
 param_distributions = {'max_features': range(1, 5), 'max_depth': range(5, 10)}
 
@@ -31,13 +17,13 @@ search = RandomizedSearchCV(estimator=tree.DecisionTreeClassifier(random_state=0
                             param_distributions=param_distributions,
                             random_state=0)
 
-search.fit(X_train, y_train)
+search.fit(X_scaled, y_train)
 
 search.best_params_
 
-search.score(X_test, y_test)
+search.score(X_scaled_test, y_test)
 
-predictions = search.predict(X_test)
+predictions = search.predict(X_scaled_test)
 
 balanced_accuracy_score(y_test, predictions)
 matthews_corrcoef(y_test, predictions)
