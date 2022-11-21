a5b06e50f0a163fdd72d341569ce8ce2586eeb56
Seth Berry <seth.berry@nd.edu>
2022-11-21 15:35:04 -0500

    01_03 skeleton

--- code/chapter_01/chapter_01_video_03.py
+++ code/chapter_01/chapter_01_video_03.py
@@ -1,1 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_breast_cancer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, roc_curve, auc
+
+X, y = load_breast_cancer(return_X_y=True)
+
+clf = LogisticRegression(solver="liblinear", random_state=0).fit(X, y)
+
+roc_auc_score(y, clf.predict_proba(X)[:, 1])
+
+roc_auc_score(y, clf.predict(X))
