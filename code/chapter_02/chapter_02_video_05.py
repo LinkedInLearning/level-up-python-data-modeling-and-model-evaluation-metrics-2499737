fc28ec29b92f8a7df7887bef1804f46608b8823b
Seth Berry <seth.berry@nd.edu>
2022-11-21 06:23:14 -0500

    streamline 02_05

--- code/chapter_02/chapter_02_video_05.py
+++ code/chapter_02/chapter_02_video_05.py
@@ -8,7 +8,7 @@ X_train_scaled, X_test_scaled, y_train, y_test = load(
   )
 
 svc_params = {
-  'C': np.linspace(.1, 5, 5), 'kernel': ['linear', 'rbf']
+  'C': np.linspace(.1, 5, 3), 'kernel': ['linear', 'rbf']
   }
 
 grid_search = GridSearchCV(
