316bb001e4eb6b0a6668ea8c696e4344c325766e
Seth Berry <seth.berry@nd.edu>
2022-11-21 21:31:06 -0500

    02_12 package update

--- code/chapter_02/chapter_02_video_12.py
+++ code/chapter_02/chapter_02_video_12.py
@@ -1,10 +1,9 @@
+from joblib import load, dump
 from imblearn.combine import SMOTEENN
 import numpy as np
 import pandas as pd
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
