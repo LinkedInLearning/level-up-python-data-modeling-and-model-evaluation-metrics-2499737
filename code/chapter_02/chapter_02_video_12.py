8875c333533a53ba08cb935a72ca870423375b8d
Seth Berry <seth.berry@nd.edu>
2022-11-21 21:34:16 -0500

    load confusion_matrix in 02_12

--- code/chapter_02/chapter_02_video_12.py
+++ code/chapter_02/chapter_02_video_12.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
